3ac8d7c3dc2e859438b7cc23f881ec4f9a0db6f3
Brittany Johnson <brittanyjohnson16@yahoo.com>
2021-08-18 09:41:47 -0500

    adding some files to help with deployment

--- app.py
+++ app.py
@@ -17,3 +17,7 @@ def read_root_healthcheck():
 def read_item(twisty: int, cuddly: int, pushy: int, greasy: int, zappy: int):
     # XXX: Use model here
     return {"twisty": twisty, "cuddly": cuddly, "pushy": pushy, "greasy": greasy, "zappy": zappy}
+
+if __name__ == '__main__':
+
+    uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)
